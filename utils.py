049b42b30f3d86f78485083a6ebc4c2e0cb329f4
seongjunkang123 <seongjunk123@gmail.com>
2025-12-25 19:44:09 -0800

    utils python file to get current version of model

---
+++ utils.py
@@ -0,0 +1,14 @@
+import os
+
+def get_version():
+    gen_model_weights_directory = './gen_model_weights'
+    sorted_files = sorted(os.listdir(gen_model_weights_directory))
+
+    last_index = len(sorted_files) - 1
+    if last_index < 0:
+        v = 1
+    else:
+        file = sorted_files[last_index]
+        v = int(file[5]) + 1
+
+    return v
