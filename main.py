74cec74a75551a11dd5abe1a2e4866c6e5451fe9
seongjunkang123 <seongjunk123@gmail.com>
2025-12-25 19:45:14 -0800

    finished data normalization, both model structure (simple to start), callbacks, and saves weights, performance, and structure info

--- main.py
+++ main.py
@@ -1,28 +1,21 @@
-import keras
+import os, datetime, pandas as pd, keras, pickle
 import tensorflow as tf
 from keras import Model
 from tensorflow.keras import layers
-from tensorflow.keras.utils import to_categorical
-
-import pandas as pd
+from tensorflow.keras.utils import to_categorical, plot_model
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.losses import BinaryCrossentropy
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+from CTGAN import CTGAN
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler
-
-import os
+from utils import get_version
 
 # define global variables (manipulate them)
 BATCH_SIZE = 16
 EPOCHS = 100
 
 # get model version
-gen_model_weights_directory = './gen_model_weights'
-sorted_files = sorted(os.listdir(gen_model_weights_directory))
-
-last_index = len(sorted_files) - 1
-if last_index < 0:
-    v = 1
-else:
-    file = sorted_files[last_index]
-    v = int(file[5]) + 1
+v = get_version()
 
 # loading dataset (csv file)
 dataset_csv = pd.read_csv('../sip_data/res1/combined_data.csv')
@@ -35,12 +28,14 @@ label = dataset_csv['Disease'].values
 # 0: Asthma
 # 1: Bronchi
 # 2: COPD
-label_encoded = LabelEncoder().fit_transform(label)
+label_encoder = LabelEncoder()
+label_encoded = label_encoder.fit_transform(label)
 label_onehot = to_categorical(label_encoded, num_classes=3)
 
 # data normalization (minmaxscaling)
 # could do StandardScaler in sklearn
-data_normalized = MinMaxScaler().fit_transform(data)
+minmaxscaler = MinMaxScaler()
+data_normalized = minmaxscaler.fit_transform(data)
 print(data_normalized)
 
 # create tensorflow dataset object + shuffle
@@ -80,3 +75,67 @@ def build_discriminator():
 
 generator = build_generator()
 discriminator = build_discriminator()
+
+# define paths for models
+GEN_WEIGHTS_PATH    = f"./gen_model_weights/gen_v{v}.keras"
+GEN_INFO_PATH       = f"./gen_model_info/gen_v{v}.txt"
+GEN_INFO_IMG_PATH   = f"./gen_model_info/gen_v{v}.png"
+
+DIS_WEIGHTS_PATH    = f"./dis_model_weights/gen_v{v}.keras"
+DIS_INFO_PATH       = f"./dis_model_info/gen_v{v}.txt"
+DIS_INFO_IMG_PATH   = f"./dis_model_info/gen_v{v}.png"
+
+GAN_WEIGHTS_PATH    = f"./gan_model_weights/gan_v{v}.keras"
+GAN_PERF_PATH       = f"./gan_model_performance/gan_v{v}.pkl"
+
+# save model information
+with open(GEN_INFO_PATH, 'w') as f:
+    f.write(f"gen_v{v} info\n\n")
+
+    f.write(f"Latent Dimension: {LATENT_DIM}\n")
+    f.write(f"Number of Classes: {NUM_CLASS}\n")
+    f.write(f"Number of VOCs: {NUM_VOCS}\n")
+    f.write(f"Batch Size: {BATCH_SIZE}\n\n")
+
+    generator.summary(print_fn=lambda x: f.write(x + '\n'))
+
+with open(DIS_INFO_PATH, 'w') as f:
+    f.write(f"dis_v{v} info\n\n")
+    discriminator.summary(print_fn=lambda x: f.write(x + '\n'))
+
+# save model info as image
+plot_model(generator, to_file=GEN_INFO_IMG_PATH,
+           show_shapes=True, show_layer_names=True, dpi=96)
+plot_model(discriminator, to_file=DIS_INFO_IMG_PATH,
+           show_shapes=True, show_layer_names=True, dpi=96)
+
+# make GAN
+gan = CTGAN(discriminator=discriminator, generator=generator, latent_dim=LATENT_DIM, num_classes=NUM_CLASS)
+
+# set optimizers and loss functions
+gan.compile(
+    g_optimizer=Adam(learning_rate=0.0002, beta_1=0.5),
+    d_optimizer=Adam(learning_rate=0.0002, beta_1=0.5),
+    loss_fn=BinaryCrossentropy()
+)
+
+# callbacks (implement later)
+early_stopping_callback = EarlyStopping(
+    patience=20,
+    restore_best_weights=True
+)
+checkpoint_callback = ModelCheckpoint(
+    filepath=GAN_WEIGHTS_PATH,
+    save_best_only=True,
+)
+
+# train model
+history = gan.fit(dataset, epochs=EPOCHS, callbacks=[early_stopping_callback, checkpoint_callback])
+
+# save weights for generator and discriminator
+discriminator.save(DIS_WEIGHTS_PATH)
+generator.save(GEN_WEIGHTS_PATH)
+
+# save performance
+with open(GAN_PERF_PATH, 'w') as f:
+    pickle.dump(history.history, f)
